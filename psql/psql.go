ff5ea9ac912209b124780bb4ea15ff1cc4cee05e
AJ Ostrow <alexander.ostrow@gmail.com>
2016-11-07 15:15:31 -0500

    move database importer into psql package

---
+++ psql/psql.go
@@ -0,0 +1,63 @@
+package psql
+
+import (
+	"github.com/aj0strow/pgschema/info"
+	"github.com/aj0strow/pgschema/tree"
+)
+
+func LoadDatabaseNode(db info.Conn) (tree.DatabaseNode, error) {
+	schemas, err := info.LoadSchemas(db)
+	if err != nil {
+		return tree.DatabaseNode{}, err
+	}
+	var schemaNodes []tree.SchemaNode
+	for _, schema := range schemas {
+		schemaNode, err := LoadSchemaNode(db, schema)
+		if err != nil {
+			return tree.DatabaseNode{}, err
+		}
+		schemaNodes = append(schemaNodes, schemaNode)
+	}
+	databaseNode := tree.DatabaseNode{
+		SchemaNodes: schemaNodes,
+	}
+	return databaseNode, nil
+}
+
+func LoadSchemaNode(db info.Conn, schema info.Schema) (tree.SchemaNode, error) {
+	tables, err := info.LoadTables(db, schema.SchemaName)
+	if err != nil {
+		return tree.SchemaNode{}, err
+	}
+	var tableNodes []tree.TableNode
+	for _, table := range tables {
+		tableNode, err := LoadTableNode(db, schema, table)
+		if err != nil {
+			return tree.SchemaNode{}, err
+		}
+		tableNodes = append(tableNodes, tableNode)
+	}
+	schemaNode := tree.SchemaNode{
+		Schema:     schema,
+		TableNodes: tableNodes,
+	}
+	return schemaNode, nil
+}
+
+func LoadTableNode(db info.Conn, schema info.Schema, table info.Table) (tree.TableNode, error) {
+	columns, err := info.LoadColumns(db, schema.SchemaName, table.TableName)
+	if err != nil {
+		return tree.TableNode{}, err
+	}
+	var columnNodes []tree.ColumnNode
+	for _, column := range columns {
+		columnNodes = append(columnNodes, tree.ColumnNode{
+			Column: column,
+		})
+	}
+	tableNode := tree.TableNode{
+		Table:       table,
+		ColumnNodes: columnNodes,
+	}
+	return tableNode, nil
+}
